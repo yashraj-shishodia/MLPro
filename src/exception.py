9e05ee8fb10861d3d57a99431074dbb359531fa9
Yashraj Shishodia <yashrajshishodia@gmail.com>
2026-08-30 15:41:31 +0530

    Fix data ingestion pipeline

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error, error_details:sys):
     _,_,exc_tb=error_details.exc_info() 
@@ -11,9 +11,9 @@ def error_message_details(error, error_details:sys):
 
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self,error_message,error_details:sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+        self.error_message=error_message_details(error_message,error_details=error_details)
     
     def __str__(self):
         return self.error_message
