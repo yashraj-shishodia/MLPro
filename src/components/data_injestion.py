8515edbc5655fe101d1f681bfabb6aff7df040dd
Yashraj Shishodia <yashrajshishodia@gmail.com>
2026-09-05 12:02:54 +0530

    Model Trainer

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from src.components.model_trainer import ModelTrainer
 from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
@@ -9,6 +10,9 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig 
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -50,5 +54,9 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation() 
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
